f07ddc2ebbeefdba34ab02e4ed503907358c42c4
JD Hiudt <jacob.d.hiudt@gmail.com>
2016-10-08 02:06:15 -0500

    update documentation, test conditional statements

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,47 +1,66 @@
-puts "What's your name?"
-userName = gets.chomp
-  if (userName == 'Drake Cula' || userName == 'Tu Fang')
-    vampire = true
-  else 
-    vampire = false
+puts "How many employees would you like to survey?"
+employees = gets.chomp.to_i
+count = 0
+
+while count <= employees
+  puts "What's your name?"
+  userName = gets.chomp
+
+  puts "How old are you?"
+  age = gets.chomp.to_i
+  puts "What year were you born?"
+  birthYear = gets.chomp.to_i
+
+  while true
+    puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+    garlic = gets.chomp.downcase
+      if (garlic == "yes" || garlic == 'no')
+        break
+      else
+        puts "Please answer either 'yes' or 'no'"
+      end
   end
 
-puts "How old are you?"
-age = gets.chomp.to_i
-puts "What year were you born?"
-birthYear = gets.chomp.to_i
-  if 2016 - age = birthYear
-    ageRight = false
-  else
-    ageRight = true
+  while true
+    puts "Would you like to enroll in the company’s health insurance?"
+    healthIns = gets.chomp.downcase
+      if (healthIns == "yes" || healthIns == 'no')
+        break
+      else
+        puts "Please answer either 'yes' or 'no'"
+      end
   end
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic = gets.chomp
-  if garlic == 'yes'
-    likesGarlic = true
-  else
-    likesGarlic = false
+  puts "Name any allergies you have"
+  while true
+    allergy = gets.chomp.downcase
+    if (allergy == "sunshine" || allergy == 'done')
+      break
+    else
+      puts "Please enter another allergy or type 'done' when finished"
+    end
   end
 
-puts "Would you like to enroll in the company’s health insurance?"
-healthInsuranceSignUp = gets.chomp
-  if healthInsuranceSignUp == 'yes'
-    optIn = true
+  if (userName == "Drake Cula" || userName == "Tu Fang")
+    puts "Definitely a vampire."
+  elsif allergy == 'sunshine' 
+    puts "Probably a vampire."
+  elsif age == 2016 - birthYear && (garlic == 'yes' || healthIns == 'yes')
+    puts "Probably not a vampire."  
+  elsif age != 2016 - birthYear && garlic == "no" && healthIns == "no"
+    puts "Almost certainly a vampire"
+  elsif age != 2016 - birthYear && (garlic == 'no' || healthIns == 'no')
+    puts "Probably a vampire."
   else
-    optIn = false
-  end 
+    puts "Results inconclusive"
+  end
 
-puts
-if vampire == true
-  puts "Definately a vampire."
-elsif (ageRight = false && (likesGarlic = false || optIn = false))
-  puts "Probably a vampire."
-elsif (ageRight = true && (likesGarlic = true || optIn = true))
-  puts "Probably not a vampire."
-elsif (ageRight = false && likesGarlic = false && optIn = false)
-  puts "Almost certainly a vampire."
-else
-  puts "Results inconclusive"
+count += 1
 end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends!"
+
+
+
+
 

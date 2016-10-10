a061f7f5f574941642eccced0461633f9ea1d6ca
JD Hiudt <jacob.d.hiudt@gmail.com>
2016-10-10 01:29:58 -0500

    add if statement that considers the range of an index

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,11 +1,14 @@
 def encrypt(str)
   index = 0
-  while index >= str.length
-    str[index] = str[index].next
-    index += 1
+  while index < str.length
+  str[index] = str[index].next
+    if str[0..1] == "aa"
+      str[0..1] = str[0...1]
+    end 
+  index += 1
   end
-  p str 
-end
+  p str
+end 
 
 encrypt("abc")
 encrypt("zed")

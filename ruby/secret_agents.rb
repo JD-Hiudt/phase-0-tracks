56c8a9538fa209d713ae7f0071696ad9853ea7d9
JD Hiudt <jacob.d.hiudt@gmail.com>
2016-10-10 20:18:05 -0500

    add nested method call, add a user password encrypt/decrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -26,7 +26,6 @@ p encrypt("abc")
 p encrypt("zed")
 
 
-
 def decrypt(str)
   i = 0
   decryptString = ""
@@ -45,6 +44,35 @@ end
 
 p decrypt("bcd")
 p decrypt("afe")
+
+p decrypt(encrypt("swordfish")) 
+
+
+#ask user if they want to encrypt or decrypt
+#add condition if answer outside those options to repeat ask
+#get password from user
+#based on if they select encrypt or decrypt, run appropriate method with the password as the argument
+
+def crypticPassword
+  while true
+    puts "Do you want to encrypt or decrypt a password"
+    choice = gets.chomp.downcase 
+    if (choice == "encrypt" || choice == "decrypt")
+      puts "What is the password?"
+      password = gets.chomp 
+        if choice == "encrypt"
+          return encrypt(password)
+          break
+        else choice == "decrypt"
+          return decrypt(password) 
+        end 
+    else
+      puts "please enter 'encrypt' or 'decrypt'"
+    end
+  end
+end
+
+p crypticPassword
 
 
 

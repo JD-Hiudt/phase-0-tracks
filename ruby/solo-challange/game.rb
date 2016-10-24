56314b5bc42724b5928c7234b7a9e78fa7fb5d5f
JD Hiudt <jacob.d.hiudt@gmail.com>
2016-10-23 21:49:02 -0500

    add pseudo code for game

---
+++ ruby/solo-challange/game.rb
@@ -0,0 +1,14 @@
+#Game takes a word as an argument
+  #Break word down into individual letters
+#Game will ask for user input to guess the word(argument)
+  #Establish count of user guesses
+  #Break user word down into individual letters
+  #Compare letters of user word to word(argument)
+  #IF letters match, print letters to screen
+  #ELSE print blank space (_)
+#IF user word not equal to first work, repeat from line 3
+#Until count of user guesses equals the length of the original word(argument), countinue to run game
+#IF a word was already guessed, nothing is added to the count of guesses
+#IF count limit reached, print lose message
+#IF word guess before limit, print win message
+

49eb3a019b217889ab94e8f6113c1acdbaaefbe0
JD Hiudt <jacob.d.hiudt@gmail.com>
2016-10-23 20:24:29 -0500

    add method to delete an item

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -11,6 +11,9 @@ class TodoList
     @list.push(item_to_add)
   end
 
+  def delete_item(item_to_delete)
+    @list.delete(item_to_delete)
+  end
 end
 
 TodoList.new(["do the dishes","mow the lawn"])

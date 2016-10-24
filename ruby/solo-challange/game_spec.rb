2ea814ac7dad266e4793212470ccf89855b83f9f
JD Hiudt <jacob.d.hiudt@gmail.com>
2016-10-24 10:15:29 -0500

    switched to feature branch to test

--- ruby/solo-challange/game_spec.rb
+++ ruby/solo-challange/game_spec.rb
@@ -8,7 +8,12 @@ describe WordGame do
   end
 
   it "breaks word down into letters" do
-    expect(word.word_as_letters).to eq ['j','a','c','o','b']
+    expect(word.word_as_array).to eq ['j','a','c','o','b']
+  end
+
+  it "breaks word down into letters, user input" do
+    word.user_word_guess = "joe"
+    expect(word.user_guess_as_array).to eq ['j','o','e']
   end
 
 end

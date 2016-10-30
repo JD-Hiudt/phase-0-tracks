1c52356f5e152e6877ef9bb1f1ef148e155b1c62
JD Hiudt <jacob.d.hiudt@gmail.com>
2016-10-30 00:33:47 -0400

    created two arrays and added items to both

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var horseNames = ["Luke I Am Your Father", "Also Starring Samuel L. Jackson", "J.J. Abrams Next Reboot", "Harambe Was An Inside Job"];
+var colors = ["Mahogany", "Fuzzy Wuzzy Brown", "Chestnut", "Red Orange"];
+
+horseNames.push("Whiskey On The Rocks");
+colors.push("Eggplant");
+
+console.log(horseNames);
+console.log(colors);

2f1211d93b8fc317a06cb6451ef602bb6061df3f
JD Hiudt <jacob.d.hiudt@gmail.com>
2016-11-08 16:09:46 -0600

    create new database and execute

--- databases/kitten_maker/kitten_maker_practice.rb
+++ databases/kitten_maker/kitten_maker_practice.rb
@@ -9,10 +9,17 @@ require 'faker'
 # create SQLite3 database
 db = SQLite3::Database.new("kittens.db")
 
-
 # learn about fancy string delimiters
+create_table_cmd = <<-SQL
+  CREATE TABLE kittens(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    age INT
+  )
+SQL
 
 # create a kittens table (if it's not there already)
+db.execute(create_table_cmd)
 
 # add a test kitten
 

b8da56583f71f837faf358c6a0d503aaf91e4cff
JD Hiudt <jacob.d.hiudt@gmail.com>
2016-11-09 14:40:31 -0600

    test code

--- databases/contact_list/contact_list.rb
+++ databases/contact_list/contact_list.rb
@@ -15,7 +15,7 @@ contact_list = <<-SQL
     id INTEGER PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
-    favorite VARCHAR(255)
+    favorites VARCHAR(255)
   );
 SQL
 
@@ -26,6 +26,7 @@ db.execute(contact_list)
 def create_contact(db, first_name, last_name)
   db.execute("INSERT INTO contacts (first_name, last_name) VALUES (?, ?)", [first_name, last_name])
 end
+
 # View Contacts by First Name
 def view_contacts_by_first_name(db)
   list = db.execute("SELECT * FROM contacts ORDER BY first_name ASC")
@@ -33,6 +34,7 @@ def view_contacts_by_first_name(db)
     puts "#{contact['first_name']} #{contact['last_name']}"
   end
 end
+
 # View Contacts by Last Name
 def view_contacts_by_last_name(db)
   list = db.execute("SELECT * FROM contacts ORDER BY last_name ASC")
@@ -40,22 +42,29 @@ def view_contacts_by_last_name(db)
     puts "#{contact['last_name']}, #{contact['first_name']}"
   end
 end
+
 # Mark Contact As Favorite
-def make_contact_favorite(db, first_name)
-  db.execute("UPDATE contacts SET favorite = 'Favorite' WHERE first_name=(?)", [first_name])
-  puts "#{first_name} has been added to your favorites"
+def make_contact_favorite(db, first_name, last_name)
+  db.execute("UPDATE contacts SET favorites = 'Favorite Contact' WHERE first_name=(?)", [first_name])
+  puts "#{first_name} #{last_name} has been added to your favorites"
 end
 
 # USER INTERFACE
 
+
+
 # TEST 
 
 
-
-
+create_contact(db, 'Joseph', 'Hiudt')
+create_contact(db, 'Daniel', 'Hiudt')
+create_contact(db, 'Jacob', 'Hiudt')
+create_contact(db, 'Zac', 'Hiudt')
+create_contact(db, 'Matt', 'Hiudt')
+make_contact_favorite(db, 'Joseph', 'Hiudt')
 view_contacts_by_first_name(db)
 view_contacts_by_last_name(db)
-make_contact_favorite(db, 'Ruthe')
+
 
 
 

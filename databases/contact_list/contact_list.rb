8c34830946bcc3c0bd552b1466735c286e95bbc9
JD Hiudt <jacob.d.hiudt@gmail.com>
2016-11-08 18:29:54 -0600

    add project folder, add project file, add pseudocode

---
+++ databases/contact_list/contact_list.rb
@@ -0,0 +1,19 @@
+# require gems
+
+# create SQLite3 database
+
+# create a kittens table 
+
+# LOGIC
+
+# Add Contact
+
+# View Contacts by First Name
+
+# View Contacts by Last Name
+
+# Mark Contact As Favorite
+
+# USER INTERFACE
+
+# TEST (create list of 20 fake contact)

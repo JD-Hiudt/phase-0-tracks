45e0c04d2fa7b63d54269bc361d6de2e6a2e0536
JD Hiudt <jacob.d.hiudt@gmail.com>
2016-11-27 18:15:06 -0600

    add create date/event method

--- databases/important_dates/important_dates.rb
+++ databases/important_dates/important_dates.rb
@@ -2,28 +2,33 @@
 require 'sqlite3'
 
 # create SQLite3 database
-db = SQLite3::Database.new("contacts.db")
+db = SQLite3::Database.new("dates_events.db")
 db.results_as_hash = true
 
 # create a important dates table
 important_dates = <<-SQL
-  CREATE TABLE IF NOT EXISTS contacts (
+  CREATE TABLE IF NOT EXISTS dates_events (
     id INTEGER PRIMARY KEY,
     event_type VARCHAR(255),
     people VARCHAR(255),
     date_celebrated VARCHAR(255),
     years_celebrated VARCHAR(255)
   );
-SQL 
+SQL
 
 db.execute(important_dates)
 
 # LOGIC
 
 # add important date
-
+def add_date(db, event_type, people, date_celebrated, years_celebrated)
+  db.execute("INSERT INTO dates_events (event_type, people, date_celebrated, years_celebrated) VALUES (?, ?, ?, ?)", [event_type, people, date_celebrated, years_celebrated])
+end
 # remove important date
 
 # edit important date
 
 # USER INTERFACE
+
+
+

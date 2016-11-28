7ba740ff32891b5ea4863fb4e70b3fe84f3d0055
JD Hiudt <jacob.d.hiudt@gmail.com>
2016-11-27 18:02:33 -0600

    add directory and ruby file for important dates project, pseudocode

--- databases/important_dates/important_dates.rb
+++ databases/important_dates/important_dates.rb
@@ -1,22 +1,11 @@
 # require gems
-require 'sqlite3'
+
 
 # create SQLite3 database
-db = SQLite3::Database.new("contacts.db")
-db.results_as_hash = true
+
 
 # create a important dates table
-important_dates = <<-SQL
-  CREATE TABLE IF NOT EXISTS contacts (
-    id INTEGER PRIMARY KEY,
-    event_type VARCHAR(255),
-    people VARCHAR(255),
-    date_celebrated VARCHAR(255),
-    years_celebrated VARCHAR(255)
-  );
-SQL 
 
-db.execute(important_dates)
 
 # LOGIC
 
